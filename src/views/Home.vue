6793ceb2536c255277f110befc7e50f07e51be5b
amarcic <aleksander.marcic@gmail.com>
2023-05-15 10:52:42 +0200

    replaced static validation card with dynamic card component

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -113,40 +113,19 @@
     </div>
 
     <div class="column col-lg-4 col-6 col-md-6 col-xs-12 cardBg">
-      <div class="card" id="validationCard">
-        <div class="card-header">
-          <div class="card-title h5">Validation</div>
-          <div class="card-subtitle text-gray">Validate files</div>
-        </div>
-        <div class="card-body">
-          <table class="table table-striped table-hover">
-            <thead>
-              <tr>
-                <th>Common validation scenarios:</th>
-              </tr>
-            </thead>
-            <tbody>
-              <tr>
-                <td>Validate MEI 3.0.0</td>
-              </tr>
-              <tr>
-                <td>Validate MEI 4.0.1</td>
-              </tr>
-              <tr>
-                <td>Validate MusicXML 3.1</td>
-              </tr>
-            </tbody>
-          </table>
-          <!--<router-link to="/validation" class="btn btn-sm float-right seeAllButton">See all</router-link>-->
-          <button class="btn btn-sm float-right seeAllButton">
-            coming soon
-          </button>
-        </div>
-      </div>
+      <!-- todo: replace link "./conversions" with "./validation" -->
+      <Card
+        id="validationCard"
+        title="Validation"
+        subtitle="Validate files"
+        description="Common validation scenarios"
+        link="./conversions"
+        :contents="validationContent"
+      />
     </div>
 
     <div class="column col-lg-4 col-6 col-md-6 col-xs-12 cardBg">
-      <!-- todo: extract values for props into a config file -->
+      <!-- todo: extract values for props into a config file; replace link "./conversions" with "./configuration" -->
       <Card
         id="configurationCard"
         title="Data Configuration"
@@ -155,36 +134,6 @@
         link="./conversions"
         :contents="configContent"
       />
-      <!--<div class="card" id="configurationCard">
-        <div class="card-header">
-          <div class="card-title h5">Data Configuration</div>
-          <div class="card-subtitle text-gray">Set data profiles</div>
-        </div>
-        <div class="card-body">
-          <table class="table table-striped table-hover">
-            <thead>
-              <tr>
-                <th>Common data configurations:</th>
-              </tr>
-            </thead>
-            <tbody>
-              <tr>
-                <td>Adjust MEI Controlevents</td>
-              </tr>
-              <tr>
-                <td>Add timestamps</td>
-              </tr>
-              <tr>
-                <td>Add IDs</td>
-              </tr>
-            </tbody>
-          </table>-->
-          <!--<router-link to="/configuration" class="btn btn-sm float-right seeAllButton">See all</router-link>-->
-          <!--<button class="btn btn-sm float-right seeAllButton">
-            coming soon
-          </button>
-        </div>
-      </div>-->
     </div>
   </div>
 </template>
@@ -197,7 +146,12 @@ export default {
   name: "home",
   data() {
     return {
-      //extract configContent in a config file later
+      //todo: extract configContent in a config file later
+      validationContent: [
+        { name: "AValidate MEI 3.0.0", link: "", href: "" },
+        { name: "Validate MEI 4.0.1", link: "", href: "" },
+        { name: "Validate MusicXML 3.1", link: "", href: "" },
+      ],
       configContent: [
         { name: "Adjust MEI ControlEvents", link: "", href: "" },
         { name: "Add timestamps", link: "", href: "" },
