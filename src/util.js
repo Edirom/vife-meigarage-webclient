d38e8af8a46040114b59c8dba032b730a0a5d6e2
amarcic <aleksander.marcic@gmail.com>
2023-09-13 14:46:08 +0200

    add parser for validation options from MEI Garage API

--- src/util.js
+++ src/util.js
@@ -31,3 +31,40 @@ export const download = ({ url, fileName }, revoke = true) => {
     invalidate(url);
   }
 };
+
+// temporary utility function to parse validation options provided by MEI Garage
+// will be deprecated once new version of MEI Garage API is available
+export const parseValidationOptions = (validations) => {
+  const notationProfiles = {
+    all: "MEI All",
+    any: "MEI anyStart",
+    cmn: "MEI CMN",
+    mensural: "MEI Mensural",
+    neumes: "MEI Neumes",
+  };
+  const MEIversions = {
+    211: "2.1.1",
+    300: "3.0.0",
+    400: "4.0.0",
+    401: "4.0.1",
+  };
+  return Object.entries(validations).map(([key, value]) => {
+    const pocket = key.split("-");
+    const regxLetters = /[a-z]+/gi;
+    const regxNumbers = /[0-9]+/gi;
+    return Object.assign(
+      {
+        format: pocket[0].match(regxLetters)[0],
+        version:
+          pocket[0].search(/[0-9]/) > -1
+            ? MEIversions[pocket[0].match(regxNumbers)[0]] ||
+              "(" + pocket[0].match(regxNumbers)[0] + ")"
+            : "dev",
+        notation: pocket[1]
+          ? notationProfiles[pocket[1]] || "(" + pocket[1] + ")"
+          : "MEI all",
+      },
+      value
+    );
+  });
+};
