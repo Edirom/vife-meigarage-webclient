4df9d5f1d47d0ce22e56fa0c01328aae7730c6be
Anne Ferger <anne.ferger@uni-paderborn.de>
2025-06-17 16:39:03 +0200

    update odd api url and actually make it dynamic

--- src/store.js
+++ src/store.js
@@ -694,7 +694,8 @@ export default new createStore({
           "/" +
           state.profiler.version +
           "/modules.json";
-        uri = "https://odd-api.edirom.de/mei/4.0.1/modules.json";
+        // this hardcodes the previous lines to another string, so it needs to be removed
+        // uri = "https://odd-api.edirom.de/mei/4.0.1/modules.json";
         fetch(uri)
           .then((response) => {
             if (!response.ok) {
@@ -715,10 +716,11 @@ export default new createStore({
                   "/" +
                   module.name +
                   "/attClasses.json";
-                uri =
-                  "https://odd-api.edirom.de/mei/4.0.1/" +
-                  module.name +
-                  "/attClasses.json";
+                // this hardcodes the previous lines to another string, so it needs to be removed  
+                // uri =
+                //  "https://odd-api.edirom.de/mei/4.0.1/" +
+                //  module.name +
+                //  "/attClasses.json";
                 fetch(uri)
                   .then((response) => {
                     if (!response.ok) {
@@ -758,10 +760,11 @@ export default new createStore({
                   "/" +
                   name +
                   "/elements.json";
-                uri =
-                  "https://odd-api.edirom.de/mei/4.0.1/" +
-                  module.name +
-                  "/elements.json";
+                // this hardcodes the previous lines to another string, so it needs to be removed
+                // uri =
+                //  "https://odd-api.edirom.de/mei/4.0.1/" +
+                //  module.name +
+                //  "/elements.json";
                 fetch(uri)
                   .then((response) => {
                     if (!response.ok) {
@@ -822,8 +825,9 @@ export default new createStore({
             "/" +
             name +
             "/elements.json";
-          uri =
-            "https://odd-api.edirom.de/mei/4.0.1/" + name + "/elements.json";
+          //  this hardcodes the previous lines to another string, so it needs to be removed
+          // uri =
+          //  "https://odd-api.edirom.de/mei/4.0.1/" + name + "/elements.json";
           fetch(uri)
             .then((response) => {
               if (!response.ok) {
